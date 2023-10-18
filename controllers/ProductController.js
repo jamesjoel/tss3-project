cc8e75ed1ebfd8104f0be951ab3e5f29c2f02ccd
jamesjoel <jamesjoelj@gmail.com>
2023-10-18 17:08:43 +0530

    sorting

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -5,6 +5,22 @@ routes.get("/", async(req, res)=>{
     let result = await Product.find();
     res.send(result);
 })
+routes.get("/bycategory/:a", async(req, res)=>{
+    let result = await Product.find({ category : req.params.a });
+    res.send(result);
+})
+routes.get("/sort/price/:a", async(req, res)=>{
+    if(req.params.a == 2){
+
+        let result = await Product.find().sort({ price : 1 });
+        res.send(result);
+    }
+    if(req.params.a == 3){
+        
+        let result = await Product.find().sort({ price : -1 });
+        res.send(result);
+    }
+})
 routes.post("/", async(req, res)=>{
     console.log(req.body);
     await Product.create(req.body);
